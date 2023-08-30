e0b87e908702174f7f155b2b4ae78be09b1bfb80
batishavo <romero.martinez.carlosraul@gmail.com>
2023-08-30 17:36:51 -0600

    publicacion de la pagina en firebase

---
+++ public/js/Configurar_Clave_SSH_Git.js
@@ -0,0 +1,50 @@
+document
+  .getElementById("boton-copiar-1")
+  .addEventListener("click", function () {
+    copiar("command-1");
+  });
+document
+  .getElementById("boton-copiar-2")
+  .addEventListener("click", function () {
+    copiar("command-2");
+  });
+document
+  .getElementById("boton-copiar-3")
+  .addEventListener("click", function () {
+    copiar("command-3");
+  });
+document
+  .getElementById("boton-copiar-4")
+  .addEventListener("click", function () {
+    copiar("command-4");
+  });
+document
+  .getElementById("boton-copiar-5")
+  .addEventListener("click", function () {
+    copiar("command-5");
+  });
+document
+  .getElementById("boton-copiar-7")
+  .addEventListener("click", function () {
+    copiar("command-7");
+  });
+
+function copiar(text) {
+  let elemento = document.getElementById(text);
+
+  let seleccion = window.getSelection();
+  let rango = document.createRange();
+
+  //console.log("entra");
+  rango.selectNodeContents(elemento);
+  seleccion.removeAllRanges();
+  seleccion.addRange(rango);
+
+  try {
+    document.execCommand("copy");
+  } catch (error) {
+    console.log(error);
+  }
+
+  seleccion.removeAllRanges();
+}
